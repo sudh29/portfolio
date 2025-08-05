97a80a03680930cb4c7c604c792984309b5d3daf
sudh29 <sudhanshuiet15@gmail.com>
2025-08-05 19:45:12 +0000

    Fix for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my_portfolio_website/',
   server: {
     port: 3000,
     open: true,
